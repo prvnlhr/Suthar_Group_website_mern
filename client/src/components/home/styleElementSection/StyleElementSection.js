b4d8605bf18922bb0bb2240f006543b440aefec2
Praveen Lohar <praveenlohar.in@gmail.com>
2022-03-06 17:52:00 +0530

    some ui fixes in style element section and completion

--- client/src/components/home/styleElementSection/StyleElementSection.js
+++ client/src/components/home/styleElementSection/StyleElementSection.js
@@ -3,16 +3,21 @@ import styles from "../../../css/styleElementSection/styleElementSection.module.
 import developingBg from "../../../img/developingBg.jpg";
 import PrimaryTitle from "./PrimaryTitle";
 
+// const useScrollPosition = () => {
+//   return scrollYPos;
+// };
 
 const StyleElementSection = ({ node, aboutUsSectionRef }) => {
   const [scrollYPos, setScrollYPos] = useState(window.pageYOffset);
 
   const onScroll = () => {
-
+    // console.log(
+    //   "scrolling",
+    //   node.current.scrollTop,
+    //   node.current.scrollTop - aboutUsSectionRef.current.offsetTop
+    // );
     setScrollYPos(node.current.scrollTop);
   };
-
-
   useEffect(() => {
     if (node.current != null) {
       node.current.addEventListener("scroll", onScroll, true);
@@ -24,6 +29,7 @@ const StyleElementSection = ({ node, aboutUsSectionRef }) => {
     };
   }, []);
 
+  //   const scrollPos = useScrollPosition();
   return (
     <div className={styles.styleElementWrapper}>
       <h1
@@ -36,7 +42,11 @@ const StyleElementSection = ({ node, aboutUsSectionRef }) => {
       </h1>
       <h1
         className={styles.primaryText}
- 
+        // style={{
+        //   transform: `translate3d(-${scrollYPos / 2}px , ${
+        //     scrollYPos * 0.2
+        //   }px ,0px)`,
+        // }}
         style={{
           transform: `translateX(-${scrollYPos / 2}px)`,
         }}
@@ -45,7 +55,10 @@ const StyleElementSection = ({ node, aboutUsSectionRef }) => {
       </h1>
 
       <PrimaryTitle />
-
+      {/* <div className={styles.titleDiv}>
+        <div className={styles.titleLine}></div>
+        <p className={styles.titleText}>WE ARE ALWAYS</p>
+      </div> */}
 
       <div className={styles.imgWrapper}>
         <img className={styles.imgBg} src={developingBg} />
