a8777ff77fdd530ad8efda9b09bd076055f70142
Praveen Lohar <praveenlohar.in@gmail.com>
2022-03-08 11:45:00 +0530

    done adding project stats

---
+++ client/src/components/home/statisticsSection/ProjectsStats.js
@@ -0,0 +1,82 @@
+import React, { useEffect } from "react";
+import styles from "../../../css/statisticsSection/statisticsSection.module.css";
+
+import { useInView } from "react-intersection-observer";
+
+import { motion, useAnimation } from "framer-motion";
+const boxVariants = {
+  hidden: {
+    opacity: 1,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      // delay: 0.8,
+      staggerChildren: 0.2,
+      delayChildren: 0.3,
+      duration: 0.6,
+      ease: [0.3, 0.1, 0.3, 1],
+    },
+  },
+};
+const textVariants = {
+  hidden: { translateY: 40, opacity: 0 },
+  visible: {
+    translateY: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.8,
+      ease: [0.3, 0.1, 0.3, 1],
+    },
+  },
+};
+const lineHeadingVariants = {
+  hidden: { translateX: -200 },
+  visible: {
+    translateX: 0,
+    transition: {
+      delay: 0.8,
+      duration: 0.8,
+    },
+  },
+};
+const lineVariants = {
+  hidden: { scaleX: 0 },
+  visible: {
+    scaleX: 1,
+    transition: {
+      // delay: 0.2,
+      duration: 0.8,
+    },
+  },
+};
+const ProjectsStats = () => {
+  const { ref, inView } = useInView({
+    // triggerOnce: true,
+    threshold: 0.3,
+  });
+  const controls = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+    if (!inView) {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+  return (
+    <motion.div ref={ref} className={styles.statsDiv}>
+      <motion.p
+        initial="hidden"
+        animate={controls}
+        variants={textVariants}
+        className={styles.statsText}
+      >
+        20+
+      </motion.p>
+    </motion.div>
+  );
+};
+
+export default ProjectsStats;
