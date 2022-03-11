6452a3da8d0621136128061adef101a3b129bdca
Praveen Lohar <praveenlohar.in@gmail.com>
2022-03-11 11:45:00 +0530

    added Jagjan logo

---
+++ client/src/components/home/ourPartnersSection/JagjanLogo.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "../../../css/our_partnersSection/partnersLogo.module.css";
+
+
+const JagjanLogo = () => {
+  return (
+    
+<svg className={styles.logo} viewBox="0 0 334 251" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M121.865 124.74C121.865 148.992 141.525 168.651 165.777 168.651C165.885 168.651 165.994 168.651 166.102 168.65V190.307C165.994 190.308 165.885 190.308 165.777 190.308C129.499 190.308 100.071 160.98 99.9103 124.74H121.865Z" fill="#D9D2D2"/>
+<path d="M231.644 124.441C231.644 88.1724 202.33 58.7503 166.102 58.575V80.8299C190.204 81.0049 209.688 100.597 209.688 124.74H231.643C231.643 124.64 231.644 124.541 231.644 124.441Z" fill="#D9D2D2"/>
+<line x1="80.4401" y1="124.368" x2="253.56" y2="124.368" stroke="#8F8F8F" stroke-width="2.26368"/>
+<line x1="165.868" y1="212.06" x2="165.868" y2="38.9402" stroke="#8F8F8F" stroke-width="2.26368"/>
+<rect x="134.464" y="83.0881" width="64.0626" height="84.7549" fill="#A7A7A7" stroke="#696363" stroke-width="1.13184"/>
+<rect x="141.335" y="90.896" width="48.8825" height="68.2382" fill="white" stroke="#625F5F" stroke-width="1.13184"/>
+<ellipse cx="178.479" cy="102.243" rx="7.45836" ry="6.72578" fill="#696363"/>
+<path d="M185.937 114.332H170.547C172.302 144.354 166.418 150.974 150.149 154.195C170.892 160.312 183.928 158.897 185.937 138.078V114.332Z" fill="#696363"/>
+</svg>
+
+    
+
+
+  );
+};
+
+export default JagjanLogo;
