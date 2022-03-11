850e61bcbe0d22bb5ab726f31645f8380edc4f80
Praveen Lohar <praveenlohar.in@gmail.com>
2022-03-11 15:05:00 +0530

    added Primary title for section

---
+++ client/src/components/home/ourPartnersSection/PrimaryTitle.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import styles from "../../../css/our_partnersSection/ourPartnerSection.module.css";
+
+import { useInView } from "react-intersection-observer";
+
+import { motion, useAnimation } from "framer-motion";
+
+const textVariants = {
+  hidden: { translateX: 80, opacity: 0 },
+  visible: {
+    translateX: 0,
+    opacity: 1,
+    transition: {
+      // delay: 0.5,
+      duration: 0.8,
+      // ease: [0.6, 0.05, -0.01, 0.9],
+    },
+  },
+};
+const lineVariants = {
+  hidden: { opacity: 0, translateX: -200 },
+  visible: {
+    opacity: [0, 0.3, 0.5, 1],
+    translateX: 0,
+    transition: {
+      delay: 0.3,
+      duration: 0.8,
+    },
+  },
+};
+const PrimaryTitle = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  });
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+    if (!inView) {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+  return (
+    <div ref={ref} className={styles.primaryTitleDiv}>
+      <motion.div
+        variants={lineVariants}
+        initial="hidden"
+        animate={controls}
+        className={styles.titleLine}
+      ></motion.div>
+      <motion.p variants={textVariants} className={styles.primaryTitleText}>
+        OUR TRUSTED PARTNERS
+      </motion.p>
+    </div>
+  );
+};
+
+export default PrimaryTitle;
