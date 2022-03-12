65fa359cb65f45387c5b8c46f26dd7a06cbf3bae
Praveen Lohar <praveenlohar.in@gmail.com>
2022-03-12 09:23:00 +0530

    added ui for Our services section

---
+++ client/src/components/home/ourServicesSection/OurServicesSection.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+
+// import styles from "../../../css/ourServicesSection/ourServicesSection.module.css";
+
+// import image1 from "../../../img/ourService1.jpg";
+// import image2 from "../../../img/ourService2.jpg";
+// import image3 from "../../../img/ourService3.jpg";
+
+// import PrimaryTitle from "./PrimaryTitle";
+
+// import { motion, useAnimation } from "framer-motion";
+// import { useInView } from "react-intersection-observer";
+// const imgVariants = {
+//   hidden: { opacity: 0, scale: 1.3 },
+//   visible: {
+//     opacity: 1,
+//     scale: 1,
+//     transition: {
+//       duration: 0.8,
+//       delay: 0.2,
+//     },
+//   },
+// };
+
+const OurServicesSection = () => {
+
+
+  // const controls = useAnimation();
+  // const { ref, inView } = useInView({
+  //   triggerOnce: true,
+  //   threshold: 0.4,
+  // });
+  // useEffect(() => {
+  //   if (inView) {
+  //     controls.start("visible");
+  //   }
+  //   if (!inView) {
+  //     controls.start("hidden");
+  //   }
+  // }, [controls, inView]);
+
+
+  return (
+
+    <motion.div
+      // initial="hidden"
+      // animate={controls}
+      ref={ref}
+      className={styles.ourServicesSectionWrapper}
+    >
+
+      {/* <div className={styles.styleDiv}>
+        <PrimaryTitle />
+      </div> */}
+
+
+      {/* <div className={styles.imageWrapper}>
+        <div className={styles.imageContainer}>
+          <motion.div variants={imgVariants} className={styles.imageDiv}>
+            <img className={styles.imageELement} src={image1} />
+            <div className={styles.overlayDiv1}>
+              <p className={styles.serviceText}>MANUFACTURING</p>
+            </div>
+          </motion.div>
+        </div>
+        <div className={styles.imageContainer}>
+          <motion.div variants={imgVariants} className={styles.imageDiv}>
+            <img className={styles.imageELement} src={image2} />
+            <div className={styles.overlayDiv2}>
+              <p className={styles.serviceText}>DESIGNING</p>
+            </div>
+          </motion.div>
+        </div>
+        <div className={styles.imageContainer}>
+          <motion.div variants={imgVariants} className={styles.imageDiv}>
+            <img className={styles.imageELement} src={image3} />
+            <div className={styles.overlayDiv3}>
+              <p className={styles.serviceText}>REPAIRING</p>
+            </div>
+          </motion.div>
+        </div>
+
+      </div> */}
+    </motion.div>
+
+
+  );
+};
+
+export default OurServicesSection;
---
+++ client/src/components/home/ourServicesSection/PrimaryTitle.js
@@ -0,0 +1,82 @@
+import React, { useEffect } from "react";
+import styles from "../../../css/ourServicesSection/ourServicesSection.module.css";
+
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
+const boxVariants = {
+  hidden: {
+    // x: -1000,
+    opacity: 1,
+  },
+  visible: {
+    opacity: 1,
+    // x: 0,
+    transition: {
+      // delay: 0.5,
+      //   staggerChildren: 0.5,
+      duration: 1,
+      //   delayChildren: 0.5,
+      // ease: [0.6, 0.05, -0.01, 0.9],
+    },
+  },
+};
+const textVariants = {
+  hidden: { translateX: 40, opacity: 0 },
+  visible: {
+    translateX: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.3,
+
+      duration: 0.8,
+      ease: [0.3, 0.1, 0.3, 1],
+    },
+  },
+};
+const lineVariants = {
+  hidden: { opacity: 0, translateX: -200 },
+  visible: {
+    opacity: [0, 0.3, 0.5, 1],
+    translateX: 0,
+    transition: {
+      delay: 0.3,
+      duration: 0.8,
+      ease: [0.3, 0.1, 0.3, 1],
+    },
+  },
+};
+const PrimaryTitle = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  });
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+    if (!inView) {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+  return (
+    <motion.div
+      variants={boxVariants}
+      initial="hidden"
+      animate={controls}
+      ref={ref}
+      className={styles.titleDivPrimary}
+    >
+      <motion.div
+        variants={lineVariants}
+        className={styles.titleLine}
+      ></motion.div>
+      <motion.p variants={textVariants} className={styles.primaryTitleText}>
+        OUR SERVICES
+      </motion.p>
+    </motion.div>
+  );
+};
+
+export default PrimaryTitle;
