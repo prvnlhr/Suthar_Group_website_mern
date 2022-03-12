5beeb2d6decfd17747e1f6c7df6c68f1ffced5c4
Praveen Lohar <praveenlohar.in@gmail.com>
2022-03-12 20:07:00 +0530

    changed bg image

--- client/src/components/home/ourServicesSection/OurServicesSection.js
+++ client/src/components/home/ourServicesSection/OurServicesSection.js
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from "react";
 // import styles from "../../../css/ourServicesSection/ourServicesSection.module.css";
 
 import image1 from "../../../img/ourService1.jpg";
-// import image2 from "../../../img/ourService2.jpg";
-// import image3 from "../../../img/ourService3.jpg";
+import image2 from "../../../img/ourService2.jpg";
+import image3 from "../../../img/ourService3.jpg";
 
 import PrimaryTitle from "./PrimaryTitle";
 
