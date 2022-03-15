ecbc9943f71483b5c94e7a33d05cf7fc07300e70
Praveen Lohar <praveenlohar.in@gmail.com>
2022-03-15 18:42:00 +0530

    completed ui for contact us section in home component

--- client/src/components/home/contactUsSection/ContactUsSection.js
+++ client/src/components/home/contactUsSection/ContactUsSection.js
@@ -17,6 +17,7 @@ const imgVariants = {
       delayChildren: 0.5,
       duration: 0.8,
       delay: 0.2,
+      // ease: [0.6, 0.05, -0.01, 0.9],
     },
   },
 };
@@ -24,19 +25,24 @@ const ContactUsSection = ({ contactUsSectionRef }) => {
   return (
     <div className={styles.contactUsSectionWrapper} ref={contactUsSectionRef}>
       <div className={styles.styleDiv}>
-
         <PrimaryTitle />
-
       </div>
 
       <div className={styles.cardImgWrapper}>
         <div className={styles.contactUsCard}>
-
           <SecondaryTitle />
-
         </div>
-
         <ContactUsBgImg />
+        {/* <motion.div
+          variants={imgVariants}
+          initial="hidden"
+          animate={controls}
+          className={styles.imageWrapper}
+          ref={ref}
+        >
+          <div className={styles.overlayDiv}></div>
+          <img className={styles.bgImage} src={contactUsBg} />
+        </motion.div> */}
       </div>
     </div>
   );
