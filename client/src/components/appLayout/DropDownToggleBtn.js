c21d5be0b1f60c7a65953a9f83184d94b91a17aa
Praveen Lohar <praveenlohar.in@gmail.com>
2022-03-16 17:52:00 +0530

    added dynamic toggle btn for drop down

---
+++ client/src/components/appLayout/DropDownToggleBtn.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "../../css/btnStyles/dropDownBtn.module.css";
+import { setDropDownShow } from "../../actions/auxillaryActions/auxillaryActions";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Route,
+  NavLink,
+  useHistory,
+  Switch,
+  useLocation,
+} from "react-router-dom";
+
+const DropDownToggleBtn = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+
+  const dropDownShow = useSelector(
+    (state) => state.auxillaryReducer.dropDownShow
+  );
+
+  const toggleDropDownMenu = () => {
+    if (dropDownShow === true) {
+      dispatch(setDropDownShow(false));
+    } else if (dropDownShow === false) {
+      dispatch(setDropDownShow(true));
+    }
+  };
+
+  return (
+    <div
+      className={
+        location.pathname === "/"
+          ? styles.menuIconWrapper
+          : styles.menuIconWrapperRelative
+      }
+      onClick={toggleDropDownMenu}
+    >
+      <div
+        className={dropDownShow === true ? styles.xLineOne : styles.lineOne}
+      ></div>
+      <div
+        className={dropDownShow === true ? styles.xLineTwo : styles.lineTwo}
+      ></div>
+    </div>
+  );
+};
+
+export default DropDownToggleBtn;
