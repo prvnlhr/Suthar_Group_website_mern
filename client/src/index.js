f8ad46ce8566f612eabc0e2536ae614e6f785c1a
Praveen Lohar <praveenlohar.in@gmail.com>
2022-03-23 09:23:00 +0530

    update index.js and index.css

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { store } from "./store/index";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
