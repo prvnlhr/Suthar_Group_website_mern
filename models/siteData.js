1d24f160f097571596613ab949930359e07f9187
Praveen Lohar <praveenlohar.in@gmail.com>
2022-10-30 10:21:00 +0530

    done schema for site

---
+++ models/siteData.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+
+const siteSchema = new mongoose.Schema({
+  owner: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  vishvakarmaProductList: [
+    {
+      componentName: {
+        type: String,
+        required: true,
+      },
+
+      imageUrl: {
+        type: String,
+        required: true,
+      },
+
+      cloudinary_id: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  krProductList: [
+    {
+      productName: {
+        type: String,
+        required: true,
+      },
+
+      productOrderQuantity: {
+        type: String,
+        required: true,
+      },
+      productDiameter: {
+        type: String,
+        required: true,
+      },
+      productPrice: {
+        type: String,
+        required: true,
+      },
+      imageUrl: {
+        type: String,
+        required: true,
+      },
+      cloudinary_id: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+const SiteDatabase = mongoose.model("siteDatas", siteSchema);
+
+module.exports = SiteDatabase;
