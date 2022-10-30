a7890791447eba1e2189f2da8926d60c830d425e
Praveen Lohar <praveenlohar.in@gmail.com>
2022-10-30 11:45:00 +0530

    added cloudinary multer upload code

---
+++ middleware/multerUploads.js
@@ -0,0 +1,11 @@
+const multer = require("multer");
+module.exports = multer({
+  storage: multer.diskStorage({}),
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.match(/jpeg|jpe|png|gif$i/)) {
+      cb(new Error("File type not supported"), false);
+      return;
+    }
+    cb(null, true);
+  },
+});
