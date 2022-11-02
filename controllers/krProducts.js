2ca9d3edf68133de4ef0575569c7ee2ad2bc64db
Praveen Lohar <praveenlohar.in@gmail.com>
2022-11-02 15:05:00 +0530

    refactor : final kr product add

--- controllers/krProducts.js
+++ controllers/krProducts.js
@@ -13,7 +13,7 @@ const krProductController = {
       const response = await SiteDatabase.findOne({
         _id: "61682efa71f33a6aa6eb866e",
       }).select("-password");
-      console.log("get products KR response");
+      // console.log("get products KR response");
       res.status(200).send(response);
     } catch (error) {
       res.status(404).json({ message: error.message });
