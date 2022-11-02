b9ef219829205d25f237b4a70ed1043c7772be33
Praveen Lohar <praveenlohar.in@gmail.com>
2022-11-02 16:23:00 +0530

    added cloudinay logic to add product

--- controllers/krProducts.js
+++ controllers/krProducts.js
@@ -21,8 +21,86 @@ const krProductController = {
   },
 
 
+  addProduct: async (req, res) => {
+    // console.log(
+    //   "add product controller KR request",
+    //   req.body.rib_name,
+    //   filePath
+    // );
+    const filePath = req.file.path;
+    const userId = req.user.id;
+    try {
+      const cloudinaryResponse = await cloudinary.v2.uploader.upload(filePath, {
+        folder: "kr",
+      });
+      console.log("cloudinary response_kr", cloudinaryResponse);
+      const productData = {
+        // _id: mongoose.Types.ObjectId(),
+        productName: req.body.rib_name,
+        productOrderQuantity: req.body.order_quantity,
+        productDiameter: req.body.rib_diameter,
+        productPrice: req.body.rib_price,
+        imageUrl: cloudinaryResponse.secure_url,
+        cloudinary_id: cloudinaryResponse.public_id,
+      };
+      console.log(productData);
+      // const addProductResponse = await SiteDatabase.findOneAndUpdate(
+      //   { _id: userId },
+      //   {
+      //     $push: {
+      //       krProductList: productData,
+      //     },
+      //   },
+      //   { returnOriginal: false }
+      // );
+      const addProductResponse = await SiteDatabase.findOneAndUpdate(
+        { _id: userId },
+        { $push: { krProductList: { $each: [productData], $position: 0 } } },
+        { returnOriginal: false }
+      );
+      console.log("mongodb KR product add Response ", addProductResponse);
+      res.status(200).json(addProductResponse);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ message: error.message });
+    }
+  },
 
- 
+  deleteProduct: async (req, res) => {
+    const cloudId = req.body.cloudId;
+    const productId = req.body.productId;
+    const userId = req.user.id;
+    console.log(
+      "delete product KR controller request",
+      typeof productId,
+      typeof userId,
+      cloudId
+    );
+
+    try {
+      const result = await cloudinary.v2.uploader.destroy(cloudId);
+      console.log(result);
+      const response = await SiteDatabase.findOneAndUpdate(
+        { _id: userId },
+        {
+          $pull: {
+            krProductList: {
+              _id: productId,
+            },
+          },
+        },
+        { returnOriginal: false }
+      );
+      console.log(response);
+      res.status(200).send({
+        data: response.krProductList,
+        msg: "kr_productDeleted",
+      });
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  },
+
 };
 
 module.exports = krProductController;
