4dc4b69f95ad6fd597b9e8aa135f5e73fe28d507
Praveen Lohar <praveenlohar.in@gmail.com>
2023-03-14 13:29:11 +0530

    update client side routing test 3

--- index.js
+++ index.js
@@ -32,26 +32,20 @@ https: app.use(
 
 app.use("/", require("./routes/index"));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-//   });
-// }
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html", "client", "build", "index.html"));
   });
 }
-
 
 
 app.listen(PORT, (err) => {
   if (err) {
     console.log(err.message);
   } else {
-    // console.log(`Server running on PORT :: ${PORT}`);
+    console.log(`Server running on PORT :: ${PORT}`);
   }
 });
